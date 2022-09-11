198c03868ba8130c4e9af3c9cd2e7ce1bf9bf6b4
ichangai gitundu <ichangaigitundu@gmail.com>
2022-09-11 16:52:34 +0300

    Fetching data using composition API

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,22 +1,52 @@
 <template>
   <div class="home">
     <h1>Glorious Purpose</h1>
+
+    <div v-if="error">
+      <h3>Error Loading Data.....<br/> Try again later</h3>
+    </div>
+
+    <div class="" v-if="posts.length">
+      <PostList :posts="posts"/>
+    </div>
+
+    <div class="" v-else>
+      <img src="@/assets/Ripple.gif" alt="">
+    </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import PostList from '@/components/PostList.vue'
 import { ref } from 'vue'
 
 export default {
-  name: 'HomeView',
-  setup(){
-    const posts = ref([
-      {title: 'How AI is used in advertisement', body: 'Brandex is utilizing AI to advertise products and services', id: 1 },
-      {title: 'How AI is used in Shopping', body: 'Supermarkets are now using automated checkout baskets', id: 2 },
-    ])
+    name: 'HomeView',
+    components: { PostList },
+    setup() {
+        const posts = ref() 
+        const error = ref(null)
 
-    return {posts}
-  }
+        const load = async () => {
+          try{
+            let data  = await fetch('http://localhost:3000/posts')
+            // console.log(data)
+            if(!data.ok){
+              throw Error('no data available')
+            }
+            posts.value = await data.json()
+
+          }
+          catch(err){
+            error.value = err.message
+            console.log(error.value)
+          }
+        }
+
+        load()
+        return { posts, error }
+    },
 }
 </script>
