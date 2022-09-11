f745c133a28e1bec19ebea9a11b28e7401c1bf2b
ichangai gitundu <ichangaigitundu@gmail.com>
2022-09-11 17:17:16 +0300

    Using composables to fetchAllData

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,33 +20,15 @@
 <script>
 // @ is an alias to /src
 import PostList from '@/components/PostList.vue'
-import { ref } from 'vue'
+import getPosts from '@/composables/getPosts'
 
 export default {
     name: 'HomeView',
     components: { PostList },
     setup() {
-        const posts = ref() 
-        const error = ref(null)
-
-        const load = async () => {
-          try{
-            let data  = await fetch('http://localhost:3000/posts')
-            // console.log(data)
-            if(!data.ok){
-              throw Error('no data available')
-            }
-            posts.value = await data.json()
-
-          }
-          catch(err){
-            error.value = err.message
-            console.log(error.value)
-          }
-        }
-
-        load()
-        return { posts, error }
+    const {posts, error, load} = getPosts()
+    load()
+    return { posts, error } 
     },
 }
 </script>
