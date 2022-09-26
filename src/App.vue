f4ffea6daf32881549b6391afc247ea6461accb5
ichangai gitundu <ichangaigitundu@gmail.com>
2022-09-26 15:40:46 +0300

    Basics Refs, emits, slots

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,46 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <main>
+    <h1>{{ title }}</h1>
+    <div v-if="showModal">
+      <Modal theme="sale" @close="toggleModal">
+        <template v-slot:links>
+          <a href="#">Click Here</a>
+          <a href="#">Sign up</a>
+        </template>
+        <h1>Unbelievable Give Aways</h1>
+        <p>You ain't ready for these prices</p>
+      </Modal>
+    </div>
+    <p></p>
+    <button @click="toggleModal">Show Modal</button>
+  </main>
 </template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "Glorious Purpose",
+      header: "Receive the best discounts",
+      showModal: false
+    }
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name)
+      this.$refs.name.classList.add('active')
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+      // console.log('show modal')
+    }
+  }
+}
+</script>
 
 <style>
 #app {
@@ -15,16 +51,9 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
